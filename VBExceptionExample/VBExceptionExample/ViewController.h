eb26d415087b940e14c0e8059c699466f4f3e5e3
Valeriy Bezuglyy <valnocorner@gmail.com>
2015-01-29 00:47:47 +0300

    added example object

---
+++ VBExceptionExample/VBExceptionExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VBExceptionExample
+//
+//  Created by Valeriy Bezuglyy on 29/01/15.
+//  Copyright (c) 2015 Valeriy Bezuglyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
