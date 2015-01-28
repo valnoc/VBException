98e68af40d3507eabdb627f44660411a8801b1f5
Valeriy Bezuglyy <valnocorner@gmail.com>
2015-01-29 00:54:35 +0300

    example

--- VBExceptionExample/VBExceptionExample/AppDelegate.m
+++ VBExceptionExample/VBExceptionExample/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "VBNotImplementedException.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    @throw [VBNotImplementedException exception];
+    
     return YES;
 }
 
